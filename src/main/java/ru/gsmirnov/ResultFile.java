32edfff4101dd18668e925ef0991d6bc0e625cd0
gsmirnov <artress@ngs.ru>
2020-04-25 20:46:53 +0700

    FileOutputStream. [#252488] (fix_01)

--- src/main/java/ru/gsmirnov/ResultFile.java
+++ src/main/java/ru/gsmirnov/ResultFile.java
@@ -15,22 +15,33 @@ import java.util.Arrays;
 public class ResultFile {
     private static final Logger LOG = LogManager.getLogger(ResultFile.class);
 
+    /**
+     * Entry point. Creates a matrix and puts it into the file.
+     *
+     * @param args the specified args.
+     */
     public static void main(String[] args) {
         int size = 10;
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             int[][] mult = matrix(size);
             for (int i = 0; i < size; i++) {
                 out.write(Arrays.toString(mult[i]).getBytes());
-                out.write("\n".getBytes());
+                out.write(System.lineSeparator().getBytes());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Creates multiplying matrix of the specified size.
+     *
+     * @param size the specified matrix's size.
+     * @return generated matrix of the specified size.
+     */
     public static int[][] matrix(int size) {
         int[][] matrix = new int[size][size];
-        for (int i = 0; i <size ; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 matrix[i][j] = (i + 1) * (j + 1);
             }
